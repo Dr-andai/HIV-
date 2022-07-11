7c7bf6e2185ee25c24d3c2fd1720d484b423a460
TheSPIDERshadow <68011723+TheSPIDERshadow@users.noreply.github.com>
2022-07-11 23:42:18 +0300

    More EDA

--- HIV PREV.R
+++ HIV PREV.R
@@ -1,4 +1,10 @@
-# HIV PREV 
+## HIV PREV 2021
+
+# More than 1.4 million Kenyans are living with HIV as of 2021. 
+# Children aged 0-4 years accounted for over 78,000 of the infected population, 
+# with the vulnerability of adolescent girls & young women attributed to poverty, 
+# lack of education and work opportunities.
+
 # Set working directory
 setwd("C:/Users/user/Documents/R/HIV-PREV")
 
@@ -8,7 +14,6 @@ pacman::p_load(
   here,       # relative file pathways  
   janitor,    # data cleaning and tables
   lubridate,  # working with dates
-  epikit,     # age_categories() function
   tidyverse,   # data management and visualization
   naniar   # missing data
 )
@@ -21,8 +26,36 @@ names(hiv)
 pct_miss(hiv)
 ##3.723404
 
+#Summary stats on Adult HIV prevalence
+summary(hiv$adult_15_hiv_prevalence)
+## results;  Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+##          0.200   3.350   4.300   5.713   5.850  25.700 
+
 #Visualize distribution of HIV prevalence
 hiv %>%
   ggplot(aes(adult_15_hiv_prevalence, county_name))+
   geom_col(fill = 'lightblue')
+## Homa Bay with Highest Prevalence
+
+#New HIV Infections, Adults >= 15 years
+hiv %>% 
+  ggplot(aes(new_hiv_infections_adults_15, county_name))+
+  geom_col(fill='darkred')
+
+sum(hiv$new_hiv_infections_adults_15)
+# 88,632 New Adult Infections 
+# Adolescents and young adults aged 15 -29 contribute to 61% 
+# of all new adult HIV infections.
+
+#Male Vs Female Prevalence
+genderx <- hiv %>%
+  pivot_longer(cols = c('hiv_prevalence_men', 'hiv_prevalence_women'),
+               names_to = "gender",
+               values_to = "prevalence")
+genderx
+
+ggplot(genderx, aes(gender, prevalence, fill=gender))+
+  geom_boxplot()+
+  theme(legend.position = "none")+   # remove legend (redundant)
+  labs( title = "HIV Prevalence boxplot by gender") 
 
