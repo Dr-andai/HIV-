ae6a74669dbe7519fdf9fc030b704c48b6f6b790
TheSPIDERshadow <68011723+TheSPIDERshadow@users.noreply.github.com>
2022-07-12 00:32:48 +0300

    Plots added

--- HIV PREV.R
+++ HIV PREV.R
@@ -32,12 +32,14 @@ summary(hiv$adult_15_hiv_prevalence)
 ##          0.200   3.350   4.300   5.713   5.850  25.700 
 
 #Visualize distribution of HIV prevalence
+#Plot 1
 hiv %>%
   ggplot(aes(adult_15_hiv_prevalence, county_name))+
   geom_col(fill = 'lightblue')
 ## Homa Bay with Highest Prevalence
 
 #New HIV Infections, Adults >= 15 years
+#Plot 2
 hiv %>% 
   ggplot(aes(new_hiv_infections_adults_15, county_name))+
   geom_col(fill='darkred')
@@ -48,13 +50,24 @@ sum(hiv$new_hiv_infections_adults_15)
 # of all new adult HIV infections.
 
 #Male Vs Female Prevalence
-genderx <- hiv %>%
+# Convert %prevalence to numeric
+pct_to_number <- function(x){
+  x_replace_pct <- sub("%"," ", x)
+  x_as_numeric <- as.numeric(x_replace_pct)
+}
+
+hiv[['hiv_prevalence_men']]= pct_to_number(hiv[['hiv_prevalence_men']])
+hiv[['hiv_prevalence_women']]= pct_to_number(hiv[['hiv_prevalence_women']])
+
+#Create Box plot
+#Plot 3
+gender_hiv <- hiv %>%
   pivot_longer(cols = c('hiv_prevalence_men', 'hiv_prevalence_women'),
                names_to = "gender",
                values_to = "prevalence")
-genderx
+gender_hiv
 
-ggplot(genderx, aes(gender, prevalence, fill=gender))+
+ggplot(gender_hiv, aes(x =gender,y= as.numeric( prevalence), fill=gender))+
   geom_boxplot()+
   theme(legend.position = "none")+   # remove legend (redundant)
   labs( title = "HIV Prevalence boxplot by gender") 
